16c99b3f09987f3a87b907f5e093c3ec2d8dea0f
Vipin Agrawal <58880095+vipinagrawal25@users.noreply.github.com>
2024-02-15 02:05:03 +0100

    Delete scripts/force_dist.py

--- scripts/force_dist.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-# dd=np.loadtxt("data/sigma0o001_k1.txt")
-dd=np.loadtxt(sys.argv[1])
-plt.plot(dd[0,1:],dd[1,1:],'o-')
-# dd=np.loadtxt(sys.argv[2])
-# plt.plot(dd[0,1:],dd[1,1:],'*-')
-plt.show()
